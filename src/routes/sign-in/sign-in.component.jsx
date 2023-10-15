db94b9dd13b655738d0da386da3c2cd687c92243
liviuschera <liviu.schera@gmail.com>
2023-10-15 20:43:31 +0300

    set up sign up with google prompt

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,16 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
-  return <h1>sign in</h1>;
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
+  return (
+    <>
+      <h1>SIGN IN</h1>
+      <button onClick={logGoogleUser}>Sign In with Google</button>
+    </>
+  );
 };
 
 export default SignIn;
