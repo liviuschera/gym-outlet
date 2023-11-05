78ce4f470bb6e3b2d29f86feb9ab35d2dfcd4e2d
liviuschera <liviu.schera@gmail.com>
2023-11-05 13:15:19 +0200

    add categories-preview.component

---
+++ src/components/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,27 @@
+import "./categories-preview.styles.scss";
+import { useContext } from "react";
+import { CategoriesContext } from "../../contexts/categories.context";
+import ProductCard from "../../components/product-card/product-card.component";
+
+const CategoriesPreview = () => {
+  const { categories } = useContext(CategoriesContext);
+
+  return (
+    <>
+      {Object.keys(categories).map((key) => (
+        <div key={key}>
+          <h2 className="title">{key}</h2>
+          <hr />
+          <div className="products-container">
+            {/* displaying a preview of the 1st 4 products in category */}
+            {categories[key].slice(0, 4).map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default CategoriesPreview;
