95fdb6af238e465a783412e067f95937e4da5d46
liviuschera <liviu.schera@gmail.com>
2023-11-06 21:29:49 +0200

    convert styling to styled components

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,20 +1,21 @@
-import "./directory-item.styles.scss";
+import {
+  BackgroundImage,
+  Body,
+  DirectoryItemContainer,
+} from "./directory-item.styles";
 import { Link } from "react-router-dom";
 
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
 
   return (
-    <div className="directory-item-container">
-      <div
-        className="background-image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      />
-      <Link to={`/shop/${title}`} className="directory-body-container">
+    <DirectoryItemContainer>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body to={`/shop/${title}`} className="directory-body-container">
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </Link>
-    </div>
+      </Body>
+    </DirectoryItemContainer>
   );
 };
 
